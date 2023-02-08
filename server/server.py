b53063f57b916825340ab8888679f9309eba3938
animatek <51943119+animatek38@users.noreply.github.com>
2023-02-08 20:50:39 +0100

    added time to logs

--- server/server.py
+++ server/server.py
@@ -1,4 +1,6 @@
-import socket, threading, json
+import socket, threading, json, datetime
+from colorama import Fore
+from colorama import init
 
 host = ''                                                      #LocalHost
 port = 7976                                                             #Choosing unreserved port
@@ -10,66 +12,55 @@ server.listen()
 clients = []
 nicknames = []
 
-BLACK=30
-RED=31
-GREEN=32
-YELLOW=33
-BLUE=34
-MAGENTA=35
-CYAN=36
-WHITE=37
-
-def color(text: str, textColor: int, bg: int):
-    bg = str(bg+10)
-    textColor = str(textColor)
-    result = '\x1b[0;'+textColor+';'+bg+'m' + text + '\x1b[0m'
-    return result
-
+now = datetime.datetime.now()
+now_text = now.strftime("%Y-%m-%d %H:%M")
 
 file = open('password.json')
 passwords = json.load(file)
 file.close()
 
 def logMessage(message):
+    now = datetime.datetime.now()
+    now_text = now.strftime("%Y-%m-%d %H:%M")
     f = open("log.log", 'a')
-    f.write((str(message)[1:].strip("'") + '\n'))
+    f.write("[" +(str(now_text) + "] | " + str(message)[1:].strip("'") + '\n'))
     f.close()
 
-
-def broadcast(message):
-    print(message)
-    logMessage(message)
+def broadcast(message):   
+    logMessage(message)                                             #broadcast function declaration
     for client in clients:
         client.send(message)
 
 def handle(client):                                         
     while True:
-        try:                
-            message = client.recv(1024)                               #recieving valid messages from client
+        try:                                                            #recieving valid messages from client
+            message = client.recv(1024)
             msg = message.decode().split(' ')
             if (msg[1] == '/ping'):
-                broadcast(color("[*] - TG TU EST GAY - [*]".encode(), RED, BLACK))
+                broadcast(f'{Fore.RED}[*] - TG TU EST GAY - [*]'.encode())
             if(msg[1] == '/online'):
-                listOfUser = ('---------------------------------\nonline: ' + (str(nicknames).strip('[]').replace("'", '')) + '\n---------------------------------')
-                listOfUser = color(listOfUser, RED, BLACK)
+                listOfUser = (f'{Fore.GREEN}---------------------------------\nONLINE: ' + (str(nicknames).strip('[]').replace("'", '')) + '\n---------------------------------')
+                listOfUser = (listOfUser)
                 broadcast(listOfUser.encode())
-                
-            broadcast(message)
 
+            print(message)
+            broadcast(message)
         except:                                                         #removing clients
             index = clients.index(client)
             clients.remove(client)
             client.close()
             nickname = nicknames[index]
-            broadcast('[*] - {} left!'.format(nickname).encode('ascii'))
+            broadcast(f'{Fore.RED}[*] - [{nickname}] left the channel !'.format(nickname).encode('ascii'))
             nicknames.remove(nickname)
             break
-        
+
+
+
 def receive():                                                          #accepting multiple clients
     while True:
-        print('[INFO] : Server is running and listening ...')
+        print('[' + str(now_text) + ']' + f'{Fore.GREEN}[INFO]: Server is running and listening ...')
         client, address = server.accept()
-        print("[CONNECTION] : Connected with {}".format(str(address)))       
+        print(f"{Fore.GREEN}[CONNECTION]{Fore.RESET}" + ": Connected with {}".format(str(address)))       
         client.send('NICKNAME'.encode('ascii'))
         nickname = client.recv(1024).decode('ascii')
         password = client.recv(1024).decode('ascii')
@@ -77,15 +68,15 @@ def receive():                                                          #accepti
             if(passwords[nickname] == password):
                 nicknames.append(nickname)
                 clients.append(client)
-                print("[NICK] : Nickname is '{}'".format(nickname))
-                aaaaaaaa = ("[" + nickname + "]" + " : {} joined!".format(nickname)).encode('ascii')
+                print(f"{Fore.BLUE}[NICK] : Nickname is {Fore.RED}"+'{}'.format(nickname)+rf"{Fore.RESET}")
+                aaaaaaaa = (f'{Fore.GREEN}[*] - [' + nickname + ']' + ' : {} joined the channel !'.format(nickname)).encode('ascii')
                 broadcast(aaaaaaaa)
-                # client.send('[INFO] : Connected to server!'.encode('ascii'))
+                client.send('[INFO] : Connected to server!'.encode('ascii'))
                 thread = threading.Thread(target=handle, args=(client,))
                 thread.start()
             else:
-                client.send('mauvais mot de passe'.encode('ascii'))
+                client.send(f"{Fore.RED}Wrong password".encode('ascii'))
         else:
-            client.send('mauvais pseudo'.encode('ascii'))
+            client.send(f"{Fore.RED}Wrong Username".encode('ascii'))
 
 receive()
